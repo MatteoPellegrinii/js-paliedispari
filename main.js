78cf3060bdfacdc82c992f3944da04b7d65cf731
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-13 14:32:18 +0200

    fine

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let parolautente = prompt("inserisci una parola");
-
-let sillabe =  [];
-sillabe.push(parolautente.split(""));
-
-console.log(sillabe);
-
-function name(params) {
-    
+function palindroma(parola) {
+    let parolautenteinversa = '';
+    for (let i=1; i<=parola.length; i++) {
+        parolautenteinversa += parola[parola.length-i];
+    }
+    if (parola = parolautenteinversa) {
+        return 'la parola è palindroma';
+    } else {
+        return 'la parola non è palindrroma';
+    }
 }
+
+let parola = prompt("inserisci una parola");
+
